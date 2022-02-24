782403163ad107d17688c2bf0547d023b7cc3e06
Brenda <brendamvreal@gmail.com>
2022-02-23 21:04:37 -0500

    Fix module import on file play.js to pass an object and add message on file client.js upon connection

--- client.js
+++ client.js
@@ -1,20 +1,24 @@
 // establishes a connection with the game server
 const net = require("net");
+
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost",
     port: 50541 // PORT number here,
+    
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write('Name: BEN')
+  });
   conn.on("data", (data) => {
     // code that does something when the connection is first established
     console.log("you ded cuz you idled");
   });
   return conn;
 };
-console.log("Connecting ...");
-connect();
 
 
 module.exports = { connect };
--- play.js
+++ play.js
@@ -1,4 +1,9 @@
-const connect = require("./client");
+// const connect = require("./client");
+
+const { connect } = require('./client');
+// const { setupInput } = require('./input');
+console.log('Connecting ...');
+const conn = connect();
 
 
 
