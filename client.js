8abe3ed7b7db455393036793186eea8c1e5a7746
Brenda <brendamvreal@gmail.com>
2022-02-23 21:21:06 -0500

    Test movimentation using setTimeout and passing string 'Move: up' as data within conn object

--- client.js
+++ client.js
@@ -11,10 +11,15 @@ const connect = function () {
   conn.setEncoding("utf8");
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
-    conn.write('Name: BEN')
+    conn.write('Name: BEN');
+    // setInterval(() => {
+    //   conn.write('Move: up');
+    // }, 50);
   });
+ 
   conn.on("data", (data) => {
     // code that does something when the connection is first established
+    conn.write('Move: up');
     console.log("you ded cuz you idled");
   });
   return conn;
